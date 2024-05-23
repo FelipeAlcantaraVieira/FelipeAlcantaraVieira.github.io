9884704f63beb325a43e28b81bcd37eec0cf5cc2
Felipe AV <felipealcantaravieira@hotmail.com>
2024-05-23 17:29:27 -0300

    bug timeline corrigido

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: 'Modern UI GPT3',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    description: "Modern UI/UX website using React.js.",
       image: '/images/gpt3copy.png',
       tags: ['JavaScript', 'React'],
     source: 'https://github.com/FelipeAlcantaraVieira/gpt3-web-project',
@@ -9,8 +9,8 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'Modern UI Hoobank',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    title: 'Hoobank Design',
+    description:"Modern UI/UX website using React.js & Vite with Tailwind CSS.",
     image: '/images/hoobank.png',
     tags: ['JavaScript', 'React'],
     source: 'https://github.com/FelipeAlcantaraVieira/bankUIprojecto',
@@ -30,11 +30,10 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2014, text: 'The start of my journey with technology', },
+  { year: 2020, text: 'Studied at Trybe, a Web Development course with duration of 1500 hours', },
+  { year: 2021, text: 'Worked at Icatu Seguros as a Junior Developer', },
+  { year: 2023, text: 'Started my studies at the Estácio University', },
 ];
 
 export const AcomplishmentsData = [
